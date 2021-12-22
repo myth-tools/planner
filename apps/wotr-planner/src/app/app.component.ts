93e21357bad91277ddf9dd1b19776498b9aafcd2
Christopher <24984762+christopher-kiss@users.noreply.github.com>
2021-12-22 14:09:02 +1100

    refactor: initial repository setup

--- apps/wotr-planner/src/app/app.component.ts
+++ apps/wotr-planner/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'myth-tools-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'myth-tools-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'wotr-planner';
+    title = 'wotr-planner';
 }
