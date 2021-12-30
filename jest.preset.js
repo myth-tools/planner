62b8fb2ce557d6545c3354c7fae0cdbb7ac47f75
Christopher <24984762+christopher-kiss@users.noreply.github.com>
2021-12-30 18:01:38 +1100

    ci: include coverage, add coverage threshold and hide console logs (#20)

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,14 @@
 const nxPreset = require('@nrwl/jest/preset');
 
-module.exports = { ...nxPreset };
+module.exports = {
+    ...nxPreset,
+    coverageReporters: ['html', 'text-summary'],
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: 80
+        }
+    }
+};
