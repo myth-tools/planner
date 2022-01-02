449075916b0effcef6bb689d0109acf63bc23f11
Christopher <24984762+christopher-kiss@users.noreply.github.com>
2022-01-02 14:27:01 +1100

    wip

--- apps/wotr-compiler/src/app/compiler.ts
+++ apps/wotr-compiler/src/app/compiler.ts
@@ -1,4 +1,5 @@
-import { Blueprints, Version, Options } from '@myth-tools/wotr-compiler/feature/compiler';
+import { /* Blueprints, Version, */ Options } from '@myth-tools/wotr-compiler/feature/compiler';
+import { Blueprints } from '@myth-tools/shared/feature/blueprints';
 
 export class Compiler {
     constructor(
@@ -8,14 +9,17 @@ export class Compiler {
     ) {}
 
     public async execute() {
-        const version = new Version(this.version);
-        const blueprints = new Blueprints(this.blueprints);
+        const blueprints = new Blueprints();
+
+        await blueprints.compile();
+        //const version = new Version(this.version);
+        //const blueprints = new Blueprints(this.blueprints);
 
         // Extract the game version and inject it into the app environment.ts
-        await version.extract();
+        //await version.extract();
 
         // Clean the blueprint folder and extract the blueprints from the game folder.
-        await blueprints.clean();
-        await blueprints.extract(this.foldersOfInterest);
+        //await blueprints.clean();
+        //await blueprints.extract(this.foldersOfInterest);
     }
 }
---
+++ libs/shared/feature/blueprints/README.md
@@ -0,0 +1,7 @@
+# shared-feature-blueprints
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test shared-feature-blueprints` to execute the unit tests via [Jest](https://jestjs.io).
