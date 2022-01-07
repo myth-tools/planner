62bb2fbd8fadebb1632d256bfe5b8d2dfd65383d
Christopher <24984762+christopher-kiss@users.noreply.github.com>
2022-01-08 10:21:49 +1100

    blueprint project

---
+++ apps/wotr/blueprint/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'myth-tools-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+    title = 'wotr-blueprint';
+}
