df597187a7d0a00f1da6e70b897b2fa678b80a55
Christopher <24984762+christopher-kiss@users.noreply.github.com>
2022-01-10 05:52:12 +1100

    build: refactor shared web library location

--- libs/utils/shared/web/jest.config.js
+++ libs/utils/shared/web/jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
             stringifyContentPathRegex: '\\.(html|svg)$'
         }
     },
-    coverageDirectory: '../../../../.output/coverage/libs/utils/shared/ng',
+    coverageDirectory: '../../../../.output/coverage/libs/utils/shared/web',
     transform: {
         '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular'
     },
