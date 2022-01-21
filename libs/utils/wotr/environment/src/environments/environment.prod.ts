6f14190a361bf8ee8d2355bdb3b8cc733c60c4b1
Christopher <24984762+christopher-kiss@users.noreply.github.com>
2022-01-21 12:43:08 +1100

    fix: broken file replacement and update build definitions

--- libs/utils/wotr/environment/src/environments/environment.prod.ts
+++ libs/utils/wotr/environment/src/environments/environment.prod.ts
@@ -5,9 +5,9 @@ export const environment: Environment = {
     release: '0.0.0-development',
     game: {
         directory: 'E:/Steam/steamapps/common/Pathfinder Second Adventure',
-        date: '2021-12-22T14:51:00.000Z',
-        hash: '34f0951707d3c52b611428f8db5b4710080c8b45',
-        version: '1.1.6e'
+        date: '2022-01-12T17:42:00.000Z',
+        hash: '2a63f6ea055dd5380a206fe0210e361f06662a31',
+        version: '1.1.7c'
     },
     firebase: {
         apiKey: 'AIzaSyCRsh4TCiKBZ9tFH6rJaJ_OdZt-3ckcbHI',
@@ -16,7 +16,7 @@ export const environment: Environment = {
     },
     blueprints: {
         zipFileName: 'blueprints.zip',
-        extractToFolderPath: 'blueprints',
+        extractToFolderPath: '.output/blueprints',
         outputFilePath: 'libs/assets',
         outputFileName: 'explorer'
     },
